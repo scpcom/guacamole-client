dc9ff517495fa63edcc71909b3b3e553a93dedee
scpcom <scpcom@gmx.de>
2024-06-12 23:43:42 +0200

    extensions: auth-privacyidea: PILogImplementation: use org.slf4j.Logger

--- extensions/guacamole-auth-privacyidea/src/main/java/org/apache/guacamole/auth/privacyidea/user/PILogImplementation.java
+++ extensions/guacamole-auth-privacyidea/src/main/java/org/apache/guacamole/auth/privacyidea/user/PILogImplementation.java
@@ -1,30 +1,53 @@
+/*
+ * Copyright 2023 NetKnights GmbH - lukas.matusiewicz@netknights.it
+ * - Modified
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License here:
+ * <a href="http://www.apache.org/licenses/LICENSE-2.0">License</a>
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.guacamole.auth.privacyidea.user;
 
 import org.privacyidea.IPILogger;
+import org.privacyidea.PrivacyIDEA;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PILogImplementation implements IPILogger
 {
+    /**
+     * Logger for PrivacyIDEA class.
+     */
+    private final Logger logger = LoggerFactory.getLogger(PrivacyIDEA.class);
+
     @Override
     public void log(String message)
     {
-        //System.out.println(message);
+        logger.info(message);
     }
 
     @Override
     public void error(String message)
     {
-        //System.out.println(message);
+        logger.error(message);
     }
 
     @Override
     public void log(Throwable t)
     {
-        //t.printStackTrace();
+        logger.info("privacyIDEA log", t);
     }
 
     @Override
     public void error(Throwable t)
     {
-        //t.printStackTrace();
+        logger.error("privacyIDEA error", t);
     }
 }
